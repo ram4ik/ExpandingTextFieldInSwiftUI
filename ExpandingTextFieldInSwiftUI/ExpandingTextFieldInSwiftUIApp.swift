95c40a76ac443f04ac362d08cd52f58be11cbb83
Ramill <ramill_ibragimov@icloud.com>
2022-10-03 07:51:01 +0300

    Initial Commit

---
+++ ExpandingTextFieldInSwiftUI/ExpandingTextFieldInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandingTextFieldInSwiftUIApp.swift
+//  ExpandingTextFieldInSwiftUI
+//
+//  Created by Ramill Ibragimov on 03.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpandingTextFieldInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
